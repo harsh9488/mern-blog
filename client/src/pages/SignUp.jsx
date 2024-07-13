677e7830fb6a49506cb54c9a6c41db31d61f6606
Harsh-Mishra <harshmishra2510@gmail.com>
2024-07-13 23:39:34 +0530

    create sign-up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to={"/"} className="text-4xl  font-bold dark:text-white">
+            <span className="px-2 py-1 rounded-lg text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+              Harsh's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is the demo project.You can sign up with your email and
+            passowrd or with Google
+          </p>
+        </div>
+        {/* Right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="text"
+                placeholder="name@company.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex text-sm mt-5">
+            <span>
+              Have an account?
+              <Link to="/sign-in" className="text-blue-500 ml-2">
+                Sign in
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
